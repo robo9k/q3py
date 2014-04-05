9d579bb8704ea18d3df55d0f21bc01498bfed1f4
robo9k <robo@9k.lv>
2014-04-05 20:19:44 +0200

    Improve doxygen comments in q3py_p.h

--- src/q3py_p.h
+++ src/q3py_p.h
@@ -1,32 +1,50 @@
+/**
+ * \file
+ * \brief Private header of q3py.
+ *
+ * This header mostly redefines Quake 3 defines.
+ * The Quake 3 headers are not really suited to be included
+ * (let alone partial inclusion by a minimal library like q3py).
+ */
+
 #ifndef Q3PY_PRIVATE_H
 #define Q3PY_PRIVATE_H
 
+
 /**
  * Quake 3 macro to use the cdecl calling convention (on Windows).
- * TODO: Actually define this properly.
- * \va ioq3 qcommon/q_platform.h
+ *
+ * \sa ioq3 qcommon/q_platform.h
  */
+/* TODO: Actually define this properly */
 #define QDECL
+
 
 /**
  * Quake 3 macro to define the maximum number of arguments that
  * the syscall function in the engine parses when being called
- * by an VM.
- * \va ioq3 qcommon/vm_local.h
+ * by a VM.
+ *
+ * \sa ioq3 qcommon/vm_local.h
  */
 #define MAX_VMSYSCALL_ARGS 16
+
 
 /*
  * Macro adopted from Quake 3 to return the count of items in
  * an array.
- * \va ioq3 qcommon/q_shared.h
+ *
+ * \sa ioq3 qcommon/q_shared.h
  */
-#define ARRAY_LEN(x)			(sizeof(x) / sizeof(x[0]))
+#define ARRAY_LEN(x) (sizeof(x) / sizeof(x[0]))
 
-/*
+
+/**
  * Pointer type of Quake 3's syscall function.
  */
+/* TODO: This ^ is broken in doxygen */
 typedef intptr_t (QDECL syscallptr)(intptr_t arg, ...);
+
 
 #if (defined _MSC_VER)
 #define Q3_API __declspec(dllexport)
@@ -40,6 +58,9 @@ typedef intptr_t (QDECL syscallptr)(intptr_t arg, ...);
  * \sa Q3PY_API is a variant for the q3py API.
  */
 /* TODO: Move Q3_API into private header */
+/* TODO: Document the fact that this is mainly informational,
+ *       as the exported symbols are controlled by quake3-vm.sym
+ */
 #define Q3_API
 #endif
 
@@ -47,7 +68,11 @@ typedef intptr_t (QDECL syscallptr)(intptr_t arg, ...);
  * A macro to export functions that are part of the q3py API.
  * \sa Q3_API is a variant for the Quake 3 API.
  */
+/* TODO: See note about exported symbols via Q3_API. Furthermore
+ *       the C API is currently only exposed via a Python capsule.
+ */
 #define Q3PY_API Q3_API
+
 
 #endif /* Q3PY_PRIVATE_H */
 
