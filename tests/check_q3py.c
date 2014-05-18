4fdf13b9732e458dd6865ae22daadee596b199d9
robo9k <robo@9k.lv>
2014-05-18 20:20:21 +0200

    Fix setup of check test cases

--- tests/check_q3py.c
+++ tests/check_q3py.c
@@ -3,14 +3,24 @@
 #include "../src/q3py_p.h"
 
 
-START_TEST(test_dllEntry)
+/**
+ * Calls dllEntry() directly without having setup
+ * the vmMain Python method before.
+ * This should fail with EXIT_FAILURE.
+ */
+START_TEST(test_dllEntry_noVmMainPy)
 {
 	dllEntry(NULL);
 }
 END_TEST
 
 
-START_TEST(test_vmMain)
+/**
+ * Calls vmMain() directly without having setup
+ * the vmMain Python method before.
+ * This should fail with EXIT_FAILURE.
+ */
+START_TEST(test_vmMain_noVmMainPy)
 {
 	int cmd = 0, dummyArg = 0;
 	intptr_t res = vmMain(cmd, dummyArg, dummyArg, dummyArg, dummyArg,
@@ -25,8 +35,8 @@ Suite* q3py_suite(void) {
 
 	TCase *tc_core = tcase_create("Core");
 
-	tcase_add_test(tc_core, test_dllEntry);
-	tcase_add_test(tc_core, test_vmMain);
+	tcase_add_exit_test(tc_core, test_dllEntry_noVmMainPy, EXIT_FAILURE);
+	tcase_add_exit_test(tc_core, test_vmMain_noVmMainPy, EXIT_FAILURE);
 
 	suite_add_tcase(suite, tc_core);
 
